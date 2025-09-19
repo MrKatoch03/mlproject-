8546204942e0729da22d98cbd9f2874264f73e9f
MrKatoch03 <mr.katoch03@gmail.com>
2025-09-19 10:58:53 +0530

    setup.py and requirement

---
+++ setup.py
@@ -0,0 +1,42 @@
+"""
+It is responsible for creating my ML project as a 
+package and deploy it in pypi
+Can install and use it
+
+"""
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+def get_requirements(file_path: str) -> List[str]:
+    """
+    this func will return the list of requirements
+    """
+    requirements = []
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","")for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+
+
+
+
+
+
+
+
+
+
+setup(
+    name = 'mlproject',
+    version = '0.0.1',
+    author = 'Mridul',
+    author_email='mridulkatoch12@gmail.com',
+    packages=find_packages(),
+    install_requires= get_requirements('requirements.txt')
+
+)
